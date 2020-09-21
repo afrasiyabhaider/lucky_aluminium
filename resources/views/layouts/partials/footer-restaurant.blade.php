ce4a984f67748b4c532755e73d46b48d09b81431
Afrasiyab Haider <afrasiyab555@gmail.com>
2020-09-21 18:45:52 +0500

    Credit Added

--- resources/views/layouts/partials/footer-restaurant.blade.php
+++ resources/views/layouts/partials/footer-restaurant.blade.php
@@ -1,11 +1,14 @@
 <!-- Main Footer -->
-  <footer class="no-print text-center text-info">
-    <!-- To the right -->
-    <!-- <div class="pull-right hidden-xs">
+<footer class="no-print text-center text-info">
+  <!-- To the right -->
+  <!-- <div class="pull-right hidden-xs">
       Anything you want
     </div> -->
-    <!-- Default to the left -->
-    <small>
-    	<b>{{ config('app.name', 'ultimatePOS') }} - V{{config('author.app_version')}} | Copyright &copy; {{ date('Y') }} All rights reserved.</b>
-    </small>
+  <!-- Default to the left -->
+  <small>
+    <b>{{ config('app.name', 'ultimatePOS') }} - V{{config('author.app_version')}} | Copyright &copy; {{ date('Y') }}
+      All rights reserved. Developed by <a href="https://facebook.com/arcocia">Arcocia</a>. Call Us at <a
+        href="tel:+923357997550">+92 335 799
+        7550</a></b>
+  </small>
 </footer>
